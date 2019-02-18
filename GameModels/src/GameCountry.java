286fe34a16c6edc7de6eb7e607a4b9820e25b3f8
shiv <shyam296@ymail.com>
2019-02-17 19:31:03 -0500

    Country model and iml file for the module.

---
+++ GameModels/src/GameCountry.java
@@ -0,0 +1,42 @@
+public class GameCountry {
+
+
+    String country_name;
+/*
+    Continent continent;
+*/
+    int player_id;
+    int armies;
+
+    public String getCountry_name() {
+        return country_name;
+    }
+
+    public void setCountry_name(String country_name) {
+        this.country_name = country_name;
+    }
+
+    /*public Continent getContinent() {
+        return continent;
+    }
+
+    public void setContinent(Continent continent) {
+        this.continent = continent;
+    }
+*/
+    public int getPlayer_id() {
+        return player_id;
+    }
+
+    public void setPlayer_id(int player_id) {
+        this.player_id = player_id;
+    }
+
+    public int getArmies() {
+        return armies;
+    }
+
+    public void setArmies(int armies) {
+        this.armies = armies;
+    }
+}

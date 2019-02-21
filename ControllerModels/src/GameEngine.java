ead4f0ab381c826d2fbf4428f1eec215098acf78
jil <mehtajil131@gmail.com>
2019-02-21 14:17:35 -0500

    Setting maximum and minimum player limits

---
+++ ControllerModels/src/GameEngine.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class GameEngine {
+
+    ArrayList<String> player_names = new ArrayList<>();
+
+    public void initialise() {
+
+        int i = 0;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter number of players");
+        int no_of_players = keyboard.nextInt();
+        if (no_of_players > 5) {
+            System.out.println("Maximum number of players is 5");
+        } else if (no_of_players < 2) {
+            System.out.println("Minimum number of players should be 2");
+        } else {
+            for (i = 1; i <= no_of_players; i++) {
+                System.out.println("Enter player name " + i + "\n");
+                player_names.add(keyboard.next());
+            }
+        }
+
+    }
+
+}
+
